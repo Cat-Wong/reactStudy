9a19eb152ff28e710aed85fb73b0ef2e42987ea5
王文华 <1318281415@qq.com>
2019-08-30 15:47:16 +0800

    redux 的计数器

---
+++ my-redux/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import store from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { addFn, minFn ,todoFn} from './action'
+
+const myrender = () => {
+  class App extends React.Component {
+    render () {
+      return (
+        <div>
+          <h1>{store.getState().counter}</h1>
+          <button onClick={() => store.dispatch(addFn)}>自增 </button>
+          <button onClick={() => store.dispatch(minFn)}>自减 </button>
+
+          <button onClick={() => store.dispatch(todoFn)}>text </button>
+        </div>
+      )
+    }
+  }
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+
+myrender()
+
+//监听sotre
+store.subscribe(() => {
+  console.log(store.getState())
+  myrender()
+});
+
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
